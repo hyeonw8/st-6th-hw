a3c572f70fda8ffb051ea3bbc580f79e5ced8b3d
hyeonw8 <whgusrud223@gmail.com>
2024-05-30 00:37:31 +0900

    ✨ useFetch hook - 커스텀 훅 추가

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,1 +1,32 @@
+import { useEffect, useState } from "react";
+
 // TODO: 이곳에 커스텀훅 작성하세요.
+const useFetch = (url) => {
+  const [title, setTitle] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        console.log("response:", response);
+        if (!response.ok) {
+          throw new Error("Network 오류");
+        }
+        const result = await response.json();
+        setTitle(result.title);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { title, loading, error };
+};
+
+export default useFetch;
